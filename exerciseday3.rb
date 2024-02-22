36fbbb0bce8d152437f38119cc8d0e63531dfe55
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-02-22 07:53:17 -0800

    Add exersice solution

--- exerciseday3.rb
+++ exerciseday3.rb
@@ -1,58 +1,58 @@
 require "sqlite3"
 require "tty-table"
 
-db = SQLite3::Database.open "product.db"
-db.execute "CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY, type TEXT, color TEXT, price INTEGER)"
+db = SQLite3::Database.open "car.db"
+db.execute "CREATE TABLE IF NOT EXISTS car(id INTEGER PRIMARY KEY, make TEXT, color TEXT, price INTEGER, stock Integer)"
 
 while true
   system "clear"
-  results = db.query "SELECT * FROM product"
-  header = ["type", "color", "price"]
+  results = db.query "SELECT * FROM car"
+  header = ["type", "color", "price", "stock"]
   rows = results.to_a
   table = TTY::Table.new header, rows
-  puts "PRODUCT (#{rows.length} total)"
+  puts "CAR (#{rows.length} total)"
   puts table.render(:unicode)
   puts
   print "[C]reate [R]ead [U]pdate [D]elete [Q]uit: "
   input_choice = gets.chomp.downcase
   if input_choice == "c"
-    print "First name: "
-    input_first_name = gets.chomp
-    print "Last name: "
-    input_last_name = gets.chomp
-    print "Salary: "
-    input_salary = gets.chomp.to_i
-    db.execute "INSERT INTO employees (first_name, last_name, salary, active) VALUES (?, ?, ?, ?)",
-               input_first_name, input_last_name, input_salary, 1
+    print "Make: "
+    input_make = gets.chomp
+    print "Color: "
+    input_color = gets.chomp
+    print "Price: "
+    input_price = gets.chomp.to_i
+    db.execute "INSERT INTO cars (make, color, price, stock) VALUES (?, ?, ?, ?)",
+               input_make, input_color, input_price, 1
   elsif input_choice == "r"
-    print "Employee id: "
+    print "Car id: "
     input_id = gets.chomp.to_i
-    results = db.query "SELECT * FROM employees WHERE id = ?", input_id
+    results = db.query "SELECT * FROM cars WHERE id = ?", input_id
     first_result = results.next
     puts "Id: #{first_result[0]}"
-    puts "First name: #{first_result[1]}"
-    puts "Last name: #{first_result[2]}"
-    puts "Salary: #{first_result[3]}"
-    puts "Active: #{first_result[4]}"
+    puts "Make: #{first_result[1]}"
+    puts "Model: #{first_result[2]}"
+    puts "Price: #{first_result[3]}"
+    puts "Stock: #{first_result[4]}"
     puts
     print "Press enter to continue"
     gets.chomp
   elsif input_choice == "u"
-    print "Employee id: "
+    print "Car id: "
     input_id = gets.chomp.to_i
-    print "Update active status (true or false): "
+    print "Update stock status (true or false): "
     input_active = gets.chomp
     if input_active == "true"
       input_active = 1
     else
       input_active = 0
     end
-    db.execute "UPDATE employees SET active = ? WHERE id = ?", input_active, input_id
+    db.execute "UPDATE car SET active = ? WHERE id = ?", input_stock, input_id
   elsif input_choice == "d"
-    puts "Delete employee"
-    print "Enter employee id: "
+    puts "Delete car"
+    print "Enter car id: "
     input_id = gets.chomp.to_i
-    db.execute "DELETE FROM employees WHERE id = ?", input_id
+    db.execute "DELETE FROM car WHERE id = ?", input_id
   elsif input_choice == "q"
     system "clear"
     puts "Goodbye!"
